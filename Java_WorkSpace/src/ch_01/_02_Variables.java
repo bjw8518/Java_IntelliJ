9db482dcb1232ae516ac246325262f7909aed8e8
JinWoo <bjw8518@gmail.com>
2023-04-07 08:56:39 +0900

    nado_230407
    nado_230407

---
+++ Java_WorkSpace/src/ch_01/_02_Variables.java
@@ -0,0 +1,14 @@
+package ch_01;
+
+public class _02_Variables {
+    public static void main(String[] args) {
+        String name = "사용자";
+        int hour = 15;
+
+
+        System.out.println(name+"님 배송이 시작됩니다"+hour+"시에 방문예정입니다");
+        System.out.println(name+"님 배송이 완료되었습니다");
+        int a = 10;
+
+    }
+}
