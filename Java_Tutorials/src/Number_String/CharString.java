95f36870e49090512aa23a2a520f6b4caa0cf679
JinWoo <bjw8518@gmail.com>
2023-05-15 17:45:55 +0900

    230419_commit

---
+++ Java_Tutorials/src/Number_String/CharString.java
@@ -0,0 +1,12 @@
+package Number_String;
+
+public class CharString {
+    public static void main(String[] args) {
+        System.out.println('생');
+        System.out.println("생활코딩");
+//에러        System.out.println('생활코딩');
+//에러        System.out.println("생");
+        //문자열안에 큰따옴표 넣기 \n 줄바꿈
+        System.out.println("egoing said\n\"Welcome programming world\"");
+    }
+}
---
+++ Java_Tutorials/src/Number_String/Number.java
@@ -0,0 +1,10 @@
+package Number_String;
+
+public class Number {
+    public static void main(String[] args) {
+        System.out.println(1+2);    //3
+        System.out.println(1.2+1.3);    //2.5
+        System.out.println(2*5);    //10
+        System.out.println(6/2);    //3
+    }
+}
